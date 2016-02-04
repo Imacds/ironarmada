2945c41a314b2d3800e6ae08ec04560bd13141ea
David Stark <david.stark@zarkonnen.com>
2016-02-04 13:20:02 +0100

    Health now displayed. Deleted some stuff surrounding life and death and enemies that was using unity networking.

--- project-files/Assets/Chrispin Assets/Prefabs/Crewman/Unit_Health.cs
+++ project-files/Assets/Chrispin Assets/Prefabs/Crewman/Unit_Health.cs
@@ -1,90 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.Networking;
+using BeardedManStudios.Network;
 using UnityEngine.UI;
 
-public class Unit_Health : NetworkBehaviour 
+public class Unit_Health : NetworkedMonoBehavior 
 {
 	const int INIT_HEALTH = 100;
 
-	[SyncVar (hook = "OnHealthChange")] 
 	int health = INIT_HEALTH;
 
 	Text healthText;
 
-	bool shouldDie = false;
-	public bool isDead = false;
-
-	public delegate void DieDelegate();
-	public event DieDelegate EventDie;
-
-	public delegate void RespawnDelegate();
-	public event RespawnDelegate EventRespawn;
-
-	public override void OnStartLocalPlayer ()
-	{
+	private void Awake() {
 		healthText = GameObject.Find ("HealthText").GetComponent<Text>();
-		SetHealthText();
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		CheckCondition();
 	}
 
-	void CheckCondition()
-	{
-		if (health <= 0 && !shouldDie && !isDead)
-		{
-			shouldDie = true;
-		}
 
-		if (health <= 0 && shouldDie)
-		{
-			if (EventDie != null)
-			{
-				EventDie();
-			}
-
-			shouldDie = false;
-		}
-
-		if (health > 0 && isDead)
-		{
-			if (EventRespawn != null)
-			{
-				EventRespawn();
-			}
-
-			isDead = false;
-		}
-	}
-
-	void SetHealthText()
-	{
-		if (isLocalPlayer)
-		{
+	void Update() {
+		if (IsSetup && IsOwner) {
 			healthText.text = "Health: " + health.ToString();
 		}
-
-	}
-
-	public void takeDamage(int dmg)
-	{
-		health -= dmg;
-	}
-
-	void OnHealthChange(int newHealth)
-	{
-		health = newHealth;
-
-		SetHealthText();
-		CheckCondition();
-	}
-
-	public void ResetHealth()
-	{
-		health = INIT_HEALTH;
 	}
 }
