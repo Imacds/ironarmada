46b00f199c63f153a34baff3ff36b8ad88578cc8
David Stark <david.stark@zarkonnen.com>
2016-02-04 14:01:18 +0100

    Work on bullet-crewman collision.

---
+++ project-files/Assets/Chrispin Assets/Prefabs/Bullet/Bullet_Hit.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using BeardedManStudios.Network;
+
+public class Bullet_Hit : SimpleNetworkedMonoBehavior {
+	public int damage = 15;
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("Bullet OTE");
+	}
+}
