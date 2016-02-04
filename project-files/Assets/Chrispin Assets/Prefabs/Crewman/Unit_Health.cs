acaebcb84330d0f6f50cc2956432a3ccd90793cd
David Stark <david.stark@zarkonnen.com>
2016-02-04 19:23:28 +0100

    Death is now a possibility.

--- project-files/Assets/Chrispin Assets/Prefabs/Crewman/Unit_Health.cs
+++ project-files/Assets/Chrispin Assets/Prefabs/Crewman/Unit_Health.cs
@@ -8,6 +8,7 @@ public class Unit_Health : NetworkedMonoBehavior
 	const int INIT_HEALTH = 100;
 
 	public int health = INIT_HEALTH;
+	public bool dead = false;
 
 	Text healthText;
 
@@ -20,6 +21,14 @@ public class Unit_Health : NetworkedMonoBehavior
 	void Update() {
 		if (IsSetup && IsOwner) {
 			healthText.text = "Health: " + health.ToString();
+		}
+
+		if (health <= 0 && !dead) {
+			dead = true;
+			GetComponentInChildren<SpriteRenderer> ().enabled = false;
+			transform.FindChild ("DeathParticles").GetComponent<ParticleSystem> ().Play ();
+			GetComponent<Unit_Controller> ().enabled = false;
+			GetComponent<Unit_Shooting> ().enabled = false;
 		}
 	}
 }
